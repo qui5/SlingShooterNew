2842fa4f17202ecc1f6808ae7436c6722a552e3e
Martin Leissler <martin@leissler.net>
2015-02-22 22:32:58 +0100

    Initial Slingshot script
    Added methods for reacting to mouse events with some test printing to console

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	void OnMouseEnter() {
+		print ("Enter");
+	}
+	
+	void OnMouseExit() {
+		print ("Exit");
+	}
+	
+	void OnMouseDown(){
+		print ("Down");	
+	}
+}

60df4aba961c821c6524a2b5c73df36ecab93a20
Martin Leissler <martin@leissler.net>
2015-02-23 13:29:21 +0100

    Instatiate a projectile
    When mouse is pressed while over Slingshot, a projectile is instantiated

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
+	// Fields set in the Unity Inspector pane
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	
+	// Fields set dynamically
 	private GameObject launchPoint;
-
+	private Vector3 launchPos;
+	private GameObject projectile;
+	private bool aimingMode;
+	
 	void Awake(){
 		//print ("Awake()");
 		launchPoint = GameObject.Find("LaunchPoint");
 		launchPoint.SetActive(false);
+		launchPos = launchPoint.transform.position;
 	}
-
+	
 	void OnMouseEnter() {
 		//print ("Enter");
 		launchPoint.SetActive(true);
@@ -19,5 +28,21 @@ public class Slingshot : MonoBehaviour {
 	void OnMouseExit() {
 		//print ("Exit");
 		launchPoint.SetActive(false);
+	}
+	
+	void OnMouseDown(){
+		//print ("Down");
+
+		// Player pressed mouse while over Slingshot
+		aimingMode = true;
+
+		// Instantiate a projectile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+
+		// Start it at launch position
+		projectile.transform.position = launchPos;
+
+		// Set it to kinematic for now
+		projectile.rigidbody.isKinematic = true;
 	}
 }

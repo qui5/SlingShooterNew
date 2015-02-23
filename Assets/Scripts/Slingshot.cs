60238d619f2f82ff92a653ec49cfc6becba9fdc3
Martin Leissler <martin@leissler.net>
2015-02-23 14:01:32 +0100

    Fire the Projectile
    Based on distance from launch point, the velocity of the projectile is set.

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -45,4 +45,39 @@ public class Slingshot : MonoBehaviour {
 		// Set it to kinematic for now
 		projectile.rigidbody.isKinematic = true;
 	}
+
+	void Update() {
+		// If the Slingshot is not in aiming mode, don't run this code
+		if(!aimingMode) return;
+
+		// Get the current mouse position in 2D screen coordinates
+		Vector3 mousePos = Input.mousePosition;
+		// Convert the mouse position to 3D world coordinates
+		mousePos.z = - Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos);
+
+		// Find the delta from launch position to 3D mouse position
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+		// Limit mouseDelta to the radius of the Slingshot SphereCollider
+		float maxMagnitude = GetComponent<SphereCollider>().radius;
+		if(mouseDelta.magnitude > maxMagnitude){
+			mouseDelta.Normalize();
+			mouseDelta *= maxMagnitude;
+		}
+
+		// Now move the projectile to this new position
+		projectile.transform.position = launchPos + mouseDelta;
+		
+		if(Input.GetMouseButtonUp(0)) {
+			// The mouse has been released
+			aimingMode = false;
+			// Fire off the projectile with given velocity
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			// Set the reference to the projectile to null as early as possible
+			projectile = null;
+		}
+		
+	}
 }

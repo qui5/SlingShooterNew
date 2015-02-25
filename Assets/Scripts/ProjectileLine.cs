c4defc143c0ed46d3af2760a3bca112aceb6ac73
Martin Leissler <martin@leissler.net>
2015-02-25 20:56:16 +0100

    Simplified ProjectileLine (no List of points)

--- Assets/Scripts/ProjectileLine.cs
+++ Assets/Scripts/ProjectileLine.cs
@@ -14,7 +14,8 @@ public class ProjectileLine : MonoBehaviour {
 	// Dynamic fields
 	private LineRenderer line;
 	private GameObject _poi;
-	private List<Vector3> points;
+	private Vector3 lastPoint;
+	private int pointsCount;
 
 	void Awake() {
 		S = this; // Set the singleton instnce
@@ -24,11 +25,9 @@ public class ProjectileLine : MonoBehaviour {
 		Color c1 = Color.yellow;
 		Color c2 = Color.red;
 		line.SetColors(c1,c2);
-
+		pointsCount = 0;
 		// Disable until its needed
 		line.enabled = false;
-		// Init points list
-		points = new List<Vector3>();
 	}
 
 	// A property: Looks to the outside like a field but internally calls get/set
@@ -41,7 +40,8 @@ public class ProjectileLine : MonoBehaviour {
 			if(_poi != null) {
 				// If poi was set to something (and now to something new), reset everything
 				line.enabled = false;
-				points = new List<Vector3>();
+				pointsCount = 0;
+				line.SetVertexCount(0);
 			}
 		}
 	}
@@ -72,36 +72,24 @@ public class ProjectileLine : MonoBehaviour {
 	public void AddPoint(){
 		Vector3 pt = _poi.transform.position;
 		// If the point isnt far enough from the last one, do nothing
-		if(points.Count > 0 && (pt - lastPoint).magnitude < minDist) {
+		if(pointsCount > 0 && (pt - lastPoint).magnitude < minDist) {
 			return;
 		}
 
-		if(points.Count == 0){
+		if(pointsCount == 0){
 			// If its the launch point (first)
-			points.Add(pt);
-			points.Add(pt);
-
 			line.SetVertexCount(2);
-			line.SetPosition(0, points[0]);
-			line.SetPosition(1, points[1]);
-
+			line.SetPosition(0, pt);
+			line.SetPosition(1, pt);
+			pointsCount += 2;
 			line.enabled = true;
-
 		} else {
 			// Not the first point
-			points.Add(pt);
-			line.SetVertexCount(points.Count);
-			line.SetPosition(points.Count - 1, pt);
+			pointsCount++;
+			line.SetVertexCount(pointsCount);
+			line.SetPosition(pointsCount - 1, pt);
 		}
-	}
 
-	public Vector3 lastPoint {
-		get {
-			if(points == null){
-				return Vector3.zero;
-			}
-			return points[points.Count - 1];
-		}
+		lastPoint = pt;
 	}
-
 }

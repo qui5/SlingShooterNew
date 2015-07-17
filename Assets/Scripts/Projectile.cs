76305c83f04839acbae8a338d0a7957ed8225a71
Ilka Rieger <ilkarieger@gmail.com>
2015-07-17 14:25:05 +0200

    Fixed collision script, customizable number of hits before Projectile is destroyed.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,14 +5,36 @@ public class Projectile : MonoBehaviour {
 	
 	public GameObject splashParticle;
 	public AudioClip HitSound;
-	public float hitVol = 1f;
+	public AudioClip SplashSound;
+	public float projectileVol = 1f;
 	
 	private AudioSource source;
+	private int hitNum = 0;
 
 	void OnCollisionEnter(){
 		Instantiate (splashParticle, this.gameObject.transform.position, this.gameObject.transform.rotation);
 		source = GetComponent<AudioSource>();
-		source.PlayOneShot(HitSound,hitVol);
+		source.PlayOneShot(HitSound,projectileVol);
+
+		hitNum++;
+		checkHit ();
 	}
 
+	void checkHit(){
+		if (hitNum == 3) {
+			source.PlayOneShot(SplashSound,projectileVol);
+			StartCoroutine("waitAndDestroy");
+		}
+		else return;
+	}
+
+	IEnumerator waitAndDestroy(){
+		yield return new WaitForSeconds (0.2f);
+		Destroy(this.gameObject);
+
+		GameObject[] particles = GameObject.FindGameObjectsWithTag("Particle");
+		for (int i=0; i<particles.Length; i++){
+			Destroy(particles[i]);
+		}
+	}
 }
